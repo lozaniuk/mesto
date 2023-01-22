8176a30ed7de0fd73c721f09a02897c41ec89761
lozaniuk <anastasia.lozanuk35@gmail.com>
2023-01-22 17:01:26 +0300

    отправить на ревью

--- README.md
+++ README.md
@@ -2,16 +2,15 @@
 
 ### Обзор
 
-* Figma
-* Картинки
+-   HTML
+-   CSS
+-   JavaScript
 
 **Figma**
 
-* [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+-   [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
 
-**Картинки**
+**Проект об уникальных местах России.**
+[Ссылка на GitHub Pages](https://tinypng.com/)
 
-Доставать картинки предстоит из Фигмы. Это расхожая практика, поэтому полезно потренироваться.
-Не забудьте [оптимизировать картинки](https://tinypng.com/), чтобы ваш сайт загружался быстрее.
-
-Удачи!
+**Планируется дальнейшая работа над проектом.**
---
+++ scripts/scripts.js
@@ -0,0 +1,41 @@
+let popupOpenBtn = document.querySelector(".profile__edit-button");
+let popupContainer = document.querySelector(".popup");
+let popupCloseBtn = document.querySelector(".popup__close-button");
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__input_type_name");
+let jobInput = document.querySelector(".popup__input_type_job");
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
+
+/* Открыть,закрыть */
+
+popupCloseBtn.addEventListener("click", closePopup);
+
+function openPopup() {
+    popupContainer.classList.add("popup_opened");
+}
+
+function closePopup() {
+    popupContainer.classList.remove("popup_opened");
+}
+
+/* Редактировать имя и информации о себе */
+
+popupOpenBtn.addEventListener("click", function () {
+    openPopup();
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
+});
+
+popupCloseBtn.addEventListener("click", function () {
+    closePopup();
+});
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+    closePopup(popupContainer);
+}
+
+formElement.addEventListener("submit", handleFormSubmit);
